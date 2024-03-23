b4e731cfa1005743895b5af0cd45a757f277e8ec
carolina2808 <60153934+carolina2808@users.noreply.github.com>
2024-03-23 13:17:42 -0500

    Prueba autogarding

--- src/1_datalake/autograding.py
+++ src/1_datalake/autograding.py
@@ -2,6 +2,31 @@
 
 import os
 
+databases_dir = "datalake/databases/"
+if not os.path.exists(databases_dir):
+    os.makedirs(databases_dir)
+
+downsampled_data = "datalake/downsampled_data/"
+if not os.path.exists(downsampled_data):
+    os.makedirs(downsampled_data)
+
+logs = "datalake/logs/"
+if not os.path.exists(logs):
+    os.makedirs(logs)
+
+models = "datalake/models/"
+if not os.path.exists(models):
+    os.makedirs(models)
+
+stagging = "datalake/raw/stagging/"
+if not os.path.exists(stagging):
+    os.makedirs(stagging)
+
+ingested = "datalake/raw/ingested/"
+if not os.path.exists(ingested):
+    os.makedirs(ingested)
+
+
 # test code files
 assert os.path.exists("src/1_datalake/create_datalake.py")
 assert os.path.exists("src/1_datalake/datalake_structure.txt")
