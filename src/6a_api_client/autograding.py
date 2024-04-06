7e0858ffab75abeedec48bb0a1cfc5e9e4c16056
carolina2808 <60153934+carolina2808@users.noreply.github.com>
2024-04-06 13:35:26 -0500

    Se corrigen problemas de rutas en autograding

--- src/6a_api_client/autograding.py
+++ src/6a_api_client/autograding.py
@@ -7,8 +7,8 @@ if not os.path.exists(logs):
 
 # test code files
 assert os.path.exists("config.json")
-assert os.path.exists("app.py")
+assert os.path.exists("src/6a_api_client/app.py")
 
 
 # test run
-assert os.path.exists("../../datalake/logs/api_client.log")
+assert os.path.exists("datalake/logs/api_client.log")
