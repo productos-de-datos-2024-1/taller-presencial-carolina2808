9bdbe073cc06f32a1108d0ad1932d1834b86042b
carolina2808 <60153934+carolina2808@users.noreply.github.com>
2024-04-07 18:01:08 -0500

    corrección

--- src/6b_api_server/autograding.py
+++ src/6b_api_server/autograding.py
@@ -1,14 +1,17 @@
 """Autograding script"""
 
 import os
+
+
 logs = "datalake/logs/"
 if not os.path.exists(logs):
     os.makedirs(logs)
+    
 
 # test code files
-assert os.path.exists("src/6b_api_client/config.json")
-assert os.path.exists("src/6b_api_client/app.py")
+assert os.path.exists("config.json")
+assert os.path.exists("server.py")
 
 
 # test run
-assert os.path.exists("datalake/logs/api_client.log")
+assert os.path.exists("../../datalake/logs/api_server.log")
